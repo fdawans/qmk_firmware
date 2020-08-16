796aa400784e5d6c7ce5ce480ee124d0f73e49d7
fdawans <info@floriandawans.com>
2020-08-16 16:03:16 +0200

    OneShot + TapDance + AutoShift Config

---
+++ keyboards/redox/keymaps/fdawans/config.h
@@ -0,0 +1,12 @@
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */k o
+
+
+//For Tap Dance
+#define TAPPING_TERM 200
+
+//Auto Shift
+#pragma once
+
+#define AUTO_SHIFT_TIMEOUT 175
+//#define NO_AUTO_SHIFT_SPECIAL
