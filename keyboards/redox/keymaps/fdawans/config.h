2d538bbdab62b9849b7468786c515c0b77199e4b
fdawans <info@floriandawans.com>
2020-08-22 16:46:25 +0200

    Added multi-keys chaining (to test)

--- keyboards/redox/keymaps/fdawans/config.h
+++ keyboards/redox/keymaps/fdawans/config.h
@@ -1,5 +1,9 @@
 #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+#define PREVENT_STUCK_MODIFIERS
+#undef ONESHOT_TIMEOUT
+
+//No timeout to chain modifiers (to test)
+//#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
 
 
 //For Tap Dance
